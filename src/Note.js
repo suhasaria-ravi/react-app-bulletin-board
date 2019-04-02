cf735176033ee05419f803872591e94fe59f53e6
Ravi Suhasaria <48365805+suhasaria-ravi@users.noreply.github.com>
2019-04-02 00:37:38 -0500

    initial files uploaded - state of note edit not saved yet.

---
+++ src/Note.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import {FaPencilAlt}  from 'react-icons/fa'
+import {FaTrashAlt} from 'react-icons/fa'
+import {FaSave} from 'react-icons/fa'
+
+class Note extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			editing: false
+		}
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+	}
+	edit() {
+		this.setState({
+			editing: true
+		})
+	}
+
+	remove() {
+		alert('removing note')
+	}
+
+	save() {
+		alert(this._newText.value)
+	}
+
+	renderForm() {
+		return (
+			<div className="note">
+				<form>
+				<textarea ref={input => this._newText = input}/>
+					<button onClick={this.save}><FaSave /></button>
+				</form>
+			</div>
+		)
+	}
+
+	renderDisplay() {
+		return (
+			<div className="note">
+				<p>{this.props.children}</p>
+				<span>
+					<button onClick={this.edit} id="edit"><FaPencilAlt /></button>
+					<button onClick={this.remove} id="remove"><FaTrashAlt /></button>
+				</span>
+			</div>
+		)
+	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
+	}
+
+}
+
+export default Note
+
+
+
+
+
